949069cfc0b3c338837155512c415ae404ec14c8
Sirori <tldus7519@gmail.com>
2023-09-06 11:27:33 +0900

    ✨ [feat] 헤더 레이아웃 및 스타일링 일부 작업
    반응형으로 구현하였으며, 레이아웃과 스타일링 대부분 완료
    
    반응형으로 구현하였을 때, 글자가 겹치는 경우가 있습니다.

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,3 +1,45 @@
 export default function Header() {
-  return <header />
+  return (
+    <header className="bg-black w-full h-[100px] px-[70px] text-gray200 flex flex-row justify-between items-center">
+      <ul className="content__list flex flex-row gap-x-14 text-xl">
+        <li className="logo">
+          <a href="">
+            <img src="/" alt="타잉" />
+          </a>
+        </li>
+        <li className="home max-md:hidden">
+          <a href="/">
+            홈
+          </a>
+        </li>
+        <li className="program max-md:hidden">
+          <a href="program">
+            TV 프로그램
+          </a>
+        </li>
+        <li className="movie max-md:hidden">
+          <a href="movie">
+            영화
+          </a>
+        </li>
+        <li className="live max-md:hidden">
+          <a href="live">
+            실시간 LIVE
+          </a>
+        </li>
+      </ul>
+      <ul className="profile__list flex flex-row gap-10">
+        <li>
+          <a href="">
+            <img src="" alt="검색" />
+          </a>
+        </li>
+        <li>
+          <a href="profile">
+            <img src="" alt="프로필" />
+          </a>
+        </li>
+      </ul>
+    </header>
+  )
 }
