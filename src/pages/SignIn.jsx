91aac20f142220031931c0e41404f091a154ddd3
kyouna_laptop <cherrymaru@gmail.com>
2023-09-08 00:47:17 +0900

    ✨ [Add] 로그인 페이지 코드 추가 로그인 유지 버튼 클릭 이벤트 관련 코드 및 이미지를 추가
    fixes: 네이밍 컨벤션 확인 필요

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -13,6 +13,26 @@ function SignIn() {
     password: '',
   });
 
+  const [imageSrc, setImageSrc] = useState("/assets/gray-check.svg");
+  const [color, setColor] = useState("bg-gray600");
+  const [isLoginClicked, setIsLoginClicked] = useState(false);
+  const [showPassword, setShowPassword] = useState("/assets/");
+  const [isPasswordClicked, setPasswordClicked] = useState(false);
+
+  const handleLoginClick = () => {
+      if (isLoginClicked) {
+        setImageSrc("/assets/gray-check.svg");
+        setColor("bg-gray600");
+        setIsLoginClicked(false); // 초기 상태 false 일 땐 초기 상태 이미지 src
+      } else {
+        setImageSrc("/assets/red-check.svg");
+        setColor("bg-primary");
+        setIsLoginClicked(true); // true일 땐 변경될 이미지 src
+      }
+  };
+
+ 
+
 
   const debounce = (callback, timeout = 300)=>{
     let cleanup;
@@ -69,10 +89,13 @@ function SignIn() {
            <input type="id" label="아이디" name="userID" className="h-14 bg-gray700 text-gray600 login-form px-4  w-full rounded-sm" placeholder="아이디"/>
            <input type="password" label="비밀번호" name="password" 
            className="h-14  bg-gray700 text-gray600 login-form px-4 w-full rounded-sm" placeholder="비밀번호"/>
-           <label className="flex text-left text-gray500 text-sm"><input type="checkbox" name="자동로그인" className="pr-[10px]"/>
+            <label className="flex text-left text-gray500 text-sm">
+              {/* <input type="checkbox" name="자동로그인" className="pr-[10px]" /> */}
+              <img src={imageSrc} alt="Taing logo" className="bg-gray600 rounded-full" onClick={handleLoginClick}/>
              자동로그인</label>
             <button type="submit" className="h-14 bg-primary text-white login-button w-full rounded-sm ">로그인하기</button>
-            <span className="text-gray300 text-center pt-[20px] pb-[10px]"><Link to="/findid" className="text-gray400 dark:hover:text-zinc-300 pr-[13px]">아이디 찾기  </Link>
+            <span className="text-gray300 text-center pt-[20px] pb-[10px]">
+              <Link to="/findid" className="text-gray400 dark:hover:text-zinc-300 pr-[13px]">아이디 찾기   </Link>
              | <Link to="/findpw" className="text-gray400 pl-[10px] dark:hover:text-zinc-300">비밀번호 찾기</Link></span>
         </form>
           <div className="flex justify-center pt-[30px] pl-[60px] pb-[100px] border-slate-600 dark:border-slate-200/30">
