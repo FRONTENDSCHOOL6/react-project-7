8af0aef20c12ef19a718397ef7c8bc495be7d28c
Sirori <tldus7519@gmail.com>
2023-09-07 14:54:07 +0900

    ✨ [feat] 헤더 이미지 추가 후 스타일링 변경
    로고와 검색, 프로필 이미지 추가 사항에 따른 스타일링 변경

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,54 +2,7 @@ import { useState, useEffect }  from 'react';
 import { Link } from "react-router-dom";
 import S from "./Header.module.css";
 
-// export default function Header() {
-
-//   return (
-//     <header className="bg-gradient-to-b from-gray700 to-transparent w-full h-[100px] px-[70px] text-gray200 flex flex-row items-center max-md:px-5 max-lg:px-7 max-lg:text-lg">
-//       <h1 className="logo">
-//           <Link to="#">
-//             <img src="/" alt="타잉" />
-//           </Link>
-//       </h1>
-//       <ul className="content flex flex-row justify-between gap-x-14 text-xl md:max-xl:gap-x-10 max-lg:ml-7 max-md:ml-4 ml-11 max-md:gap-x-4">
-//         <li className="home max-md:text-base">
-//           <Link href="#">
-//             홈
-//           </Link>
-//         </li>
-//         <li className="program max-md:text-base">
-//           <Link to="program">
-//             TV 프로그램
-//           </Link>
-//         </li>
-//         <li className="movie max-md:text-base">
-//           <Link to="movie">
-//             영화
-//           </Link>
-//         </li>
-//         <li className="live max-md:text-base">
-//           <Link to="live">
-//             실시간 LIVE
-//           </Link>
-//         </li>
-//       </ul>
-//       <ul className="profile flex flex-row gap-10 ml-auto max-md:gap-x-4">
-//         <li>
-//           <Link to="/">
-//             <img src="" alt="검색" />
-//           </Link>
-//         </li>
-//         <li>
-//           <Link to="profile">
-//             <img src="" alt="프로필" />
-//           </Link>
-//         </li>
-//       </ul>
-//     </header>
-//   )
-// }
-
-export default function Header() {
+function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
 
   //@ 스크롤 이벤트
@@ -80,9 +33,9 @@ export default function Header() {
       }
       ${S.header}`}
     >
-      <h1 className="logo">
+      <h1>
           <Link to="/">
-            <img src="/" alt="타잉" />
+            <img src="/assets/logo.svg" alt="타잉" />
           </Link>
       </h1>
       <ul className={S.content}>
@@ -110,15 +63,17 @@ export default function Header() {
       <ul className={S.profile}>
         <li>
           <Link to="/">
-            <img src="" alt="검색" />
+            <img src="/assets/search.png" alt="검색" className={S.profileImg} />
           </Link>
         </li>
         <li>
           <Link to="profile">
-            <img src="" alt="프로필" />
+            <img src="/assets/profile.png" alt="프로필" className={S.profileImg} />
           </Link>
         </li>
       </ul>
     </header>
   );
 }
+
+export default Header;
