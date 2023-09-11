a101306190bb53cb4be5bca748ae92672ad46644
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-09-11 23:05:23 +0900

    ♻️ [refactor] 검색 페이지 타입 추가
    검색 페이지의 일부 컴포넌트에 타입을 지정하였습니다.
    
    fixes: 추후 기능 구현 및 컴포넌트 분리로 변경 예정
    Related to #10
    
    <type>: <subject>  // => 제목
    ----------------------------- <한 줄 띄움 엔터>
    <body>             // => 본문
    ----------------------------- <한 줄 띄움 엔터>
    <footer>           // => 꼬리말

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,11 @@
-import S from "./Search.module.css"
-import { Helmet } from "react-helmet-async"
-import SearchIcon from "/assets/search.png"
-import { useState, useEffect } from "react"
-import XIcon from "/assets/x-icon.svg"
-import XCircleIcon from "/assets/x-circle-icon.svg"
+import { Helmet } from "react-helmet-async";
+import { useState, useEffect } from "react";
+
+import S from "./Search.module.css";
+import SearchIcon from "/assets/search.png";
+import XIcon from "/assets/x-icon.svg";
+import XCircleIcon from "/assets/x-circle-icon.svg";
+import { number } from "prop-types";
 
 //@ 검색창 부분 컴포넌트
 function SearchSection() {
@@ -24,14 +26,14 @@ function SearchSection() {
 				style={{ backgroundImage: SearchIcon && `url(${SearchIcon})` }}
 			/>
 		</div>
-	)
+	);
 }
 //@ 리스트 부분 컴포넌트
 function ListSection() {
 	const recentView = [
 		{ id: 1, title: "신병" },
 		{ id: 2, title: "아스날 연대기" },
-	]
+	];
 	const realtimeSearch = [
 		{ id: 1, title: "신병" },
 		{ id: 2, title: "아스달 연대기" },
@@ -43,7 +45,7 @@ function ListSection() {
 		{ id: 8, title: "아라문의 검" },
 		{ id: 9, title: "뭉쳐야 찬다2" },
 		{ id: 10, title: "힙하게" },
-	]
+	];
 	return (
 		<div className={`${S.listWrapper}`}>
 			<div className={`${S.recentViewWrapper}`}>
@@ -83,41 +85,41 @@ function ListSection() {
 				</ul>
 			</div>
 		</div>
-	)
+	);
 }
 //@ 현재 시간 표시 컴포넌트
 function CurrentTime() {
-	const now = new Date()
-	const year = now.getFullYear()
-	const month = now.getMonth() + 1 // 월은 0부터 시작하므로 +1을 해줍니다.
-	const date = now.getDate()
+	const now = new Date();
+	const year = now.getFullYear();
+	const month = now.getMonth() + 1; // 월은 0부터 시작하므로 +1을 해줍니다.
+	const date = now.getDate();
 
-	let hours = now.getHours()
-	let minutes = now.getMinutes()
+	let hours = now.getHours();
+	let minutes = now.getMinutes();
 
 	// AM/PM 표시를 위해 시간을 조정합니다.
-	const ampm = hours >= 12 ? "오후" : "오전"
+	const ampm = hours >= 12 ? "오후" : "오전";
 
 	//12시간제로 변경
-	hours %= 12
-	hours = hours ? hours : 12 //0시는 보통 "12"로 표기하기 때문에
+	hours %= 12;
+	hours = hours ? hours : 12; //0시는 보통 "12"로 표기하기 때문에
 
 	// 분이 한 자리수일 경우 앞에 "0"을 붙여줍니다.
-	minutes = minutes < 10 ? "0" + minutes : minutes
+	minutes = minutes < 10 ? "0" + minutes : minutes;
 
 	return (
 		<span className="block mt-4 mb-4 font-[0.75rem] text-[#646464]">
 			{`${year}.${month}.${date} ${ampm} ${hours}:${minutes} 기준`}
 		</span>
-	)
+	);
 }
 
 //@ 최근 검색어 컴포넌트
-function RecentView({ item }) {
+function RecentView({ view }) {
 	return (
-		<li key={item.id} className="mt-2">
+		<li key={view.id} className="mt-2">
 			<button className="whitespace-nowrap overflow-hidden text-ellipsis cursor-pointer text-[#a6a6a6] text-[1.25rem]">
-				{item.title}
+				{view.title}
 			</button>
 			<button
 				className="min-w-[1rem] min-h-[1rem] bg-no-repeat bg-[50%_50%] opacity-70 leading-[normal] text-base cursor-pointer align-middle ml-4 pb-2"
@@ -128,7 +130,7 @@ function RecentView({ item }) {
 				}}
 			></button>
 		</li>
-	)
+	);
 }
 //@ 인기 검색어 컴포넌트
 function RealtimeSearch({ item }) {
@@ -149,7 +151,7 @@ function RealtimeSearch({ item }) {
 				</h3>
 			</button>
 		</li>
-	)
+	);
 }
 
 function Search() {
@@ -165,6 +167,21 @@ function Search() {
 				</div>
 			</div>
 		</>
-	)
+	);
 }
-export default Search
+
+RecentView.propTypes = {
+	view: PropTypes.shape({
+		id: number.isRequired,
+		title: string.isRequired,
+	}),
+};
+
+RealtimeSearch.propTypes = {
+	item: PropTypes.shape({
+		id: number.isRequired,
+		title: string.isRequired,
+	}),
+};
+
+export default Search;
