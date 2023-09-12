84397f3fa6f2944dc563e128a7adc19b7e48097f
Sirori <tldus7519@gmail.com>
2023-09-12 16:36:16 +0900

    ✨ [feat] 반응형과 컴포넌트에 따른 헤더 분리
    헤더의 반응형에 따른 레이아웃 변경과 각 컴포넌트별 렌더링 되는 헤더를 분리
    
    fixes: 추후 수정 가능성 있음

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,79 +1,73 @@
-import { useState, useEffect }  from 'react';
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import S from "./Header.module.css";
 
 function Header() {
-  const [isScrolled, setIsScrolled] = useState(false);
+	const [isScrolled, setIsScrolled] = useState(false);
 
-  //@ 스크롤 이벤트
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 150) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
+	//@ 스크롤 이벤트
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 150) {
+				setIsScrolled(true);
+			} else {
+				setIsScrolled(false);
+			}
+		};
 
-    window.addEventListener('scroll', handleScroll);
+		window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-  return (
-    <header
-      className={`
-      ${
-        isScrolled ? "bg-gray800/80" : "to-transparent"
-      }
-      ${
-        isScrolled ? "saturate-100 backdrop-blur-md" : ""
-      }
+	return (
+		<header
+			className={`
+      ${isScrolled ? "bg-black/[80%]" : "to-transparent"}
+      ${isScrolled ? "saturate-100 backdrop-blur-lg" : ""}
       ${S.header}`}
-    >
-      <h1>
-          <Link to="/">
-            <img src="/assets/logo.svg" alt="타잉" />
-          </Link>
-      </h1>
-      <ul className={S.content}>
-        <li>
-          <Link href="/">
-            홈
-          </Link>
-        </li>
-        <li>
-          <Link to="program">
-            TV 프로그램
-          </Link>
-        </li>
-        <li>
-          <Link to="movie">
-            영화
-          </Link>
-        </li>
-        <li>
-          <Link to="live">
-            실시간 LIVE
-          </Link>
-        </li>
-      </ul>
-      <ul className={S.profile}>
-        <li>
-          <Link to="/">
-            <img src="/assets/search.png" alt="검색" className={S.profileImg} />
-          </Link>
-        </li>
-        <li>
-          <Link to="profile">
-            <img src="/assets/profile.png" alt="프로필" className={S.profileImg} />
-          </Link>
-        </li>
-      </ul>
-    </header>
-  );
+		>
+			<h1 className="w-[6.5%]">
+				<Link to="/">
+					<img src="/assets/logo.svg" alt="타잉" className="w-full" />
+				</Link>
+			</h1>
+			<ul className={S.content}>
+				<li>
+					<Link href="/" className={S.home}>
+						홈
+					</Link>
+				</li>
+				<li>
+					<Link to="program">TV 프로그램</Link>
+				</li>
+				<li>
+					<Link to="movie">영화</Link>
+				</li>
+				<li>
+					<Link to="live">내가 찜한 콘텐츠</Link>
+				</li>
+			</ul>
+			<ul className={S.profile}>
+				<li>
+					<Link to="search">
+						<img src="/assets/search.png" alt="검색" className={S.profileImg} />
+					</Link>
+				</li>
+				<li>
+					<Link to="profile">
+						<img
+							src="/assets/profile.png"
+							alt="프로필"
+							className={S.profileImg}
+						/>
+					</Link>
+				</li>
+			</ul>
+		</header>
+	);
 }
 
 export default Header;
