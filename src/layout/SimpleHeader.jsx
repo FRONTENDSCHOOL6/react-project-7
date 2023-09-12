4f1def730e9f4d486341a7505c7dcd76313b396a
Sirori <tldus7519@gmail.com>
2023-09-12 16:37:58 +0900

    ✨ [feat] 헤더 분리 작업
    헤더가 컴포넌트마다 다르게 분리될 수 있도록 함
    
    fixes: 추후 수정 가능성 있음

---
+++ src/layout/SimpleHeader.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import S from "./Header.module.css";
+
+function Header() {
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	//@ 스크롤 이벤트
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 150) {
+				setIsScrolled(true);
+			} else {
+				setIsScrolled(false);
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	return (
+		<header
+			className={`
+      ${isScrolled ? "bg-black/[80%]" : "to-transparent"}
+      ${isScrolled ? "saturate-100 backdrop-blur-lg" : ""}
+      ${S.header}`}
+		>
+			<h1 className="w-[6.5%]">
+				<Link to="/">
+					<img src="/assets/logo.svg" alt="타잉" className="w-full" />
+				</Link>
+			</h1>
+		</header>
+	);
+}
+
+export default Header;
