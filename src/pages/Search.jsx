033a0ea403a59b9b553d0af32126f3a09be377f9
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-09-14 08:57:17 +0900

    ✨ [feat] 검색 기능 일부 추가
    스와이퍼(페이지네이션, 내비게이션), 검색중,후 조건부 렌더링
    지역 상태 일부 전역 상태로 전환, 일부 파일 추가 및 폴더 구조 정리했습니다.
    
    fixes: 추후 컴포넌트 분리 및 리팩토링으로 구조 변경 가능성 있음
    Related to #33

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,25 +1,51 @@
 import { Helmet } from "react-helmet-async";
-import { useState, useEffect } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import S from "./Search.module.css";
-import SearchIcon from "/assets/search.png";
-import XIcon from "/assets/x-icon.svg";
-import XCircleIcon from "/assets/x-circle-icon.svg";
-import { number } from "prop-types";
-import pb from "@/api/pocketbase";
+import SearchIcon from "/assets/search/search.png";
+import XCircleIcon from "/assets/search/x-circle-icon.svg";
 import { getPbImageURL } from "@/utils/getPbImageURL";
+import useSearchStore from "../store/useSearchStore";
+import debounce from "../utils/debounce";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import HighlightedText from "../components/search/util/HighlightedText";
+import CurrentTime from "../components/search/util/CurrentTime";
+import RecentView from "../components/search/contents/RecentView";
+import RealtimeSearch from "../components/search/contents/RealtimeSearch";
 
 //@ 검색창 부분 컴포넌트
-function SearchSection({ searchData, setSearchData, setIsSearching }) {
+function SearchSection() {
+	const {
+		searchData,
+		setSearchData,
+		fetchData,
+		setHasSearched,
+		hasSearched,
+		addRecentSearch,
+	} = useSearchStore();
+
 	const handleInputChange = (e) => {
-		setSearchData(e.target.value);
+		const query = e.target.value;
+		setSearchData(query);
+		setHasSearched(false);
+		debounce(async () => {
+			if (!hasSearched) {
+				fetchData(query);
+			}
+		}, 300)();
 	};
 
-	const handleSearch = (e) => {
+	const handleSearch = async (e) => {
 		e.preventDefault();
-		setIsSearching(true);
+		setHasSearched(true);
+		await fetchData(searchData);
+		addRecentSearch(searchData);
 	};
+
 	return (
 		<div className={`${S.searchWrapper}`}>
 			<form onSubmit={handleSearch} className="flex justify-between  w-full">
@@ -46,10 +72,10 @@ function SearchSection({ searchData, setSearchData, setIsSearching }) {
 }
 //@ 리스트 부분 컴포넌트
 function ListSection() {
-	const recentView = [
-		{ id: 1, title: "신병" },
-		{ id: 2, title: "아스날 연대기" },
-	];
+	const recentSearches = useSearchStore((state) => state.recentSearches);
+	const clearRecentSearches = useSearchStore(
+		(state) => state.clearRecentSearches
+	);
 	const realtimeSearch = [
 		{ id: 1, title: "신병" },
 		{ id: 2, title: "아스달 연대기" },
@@ -64,24 +90,29 @@ function ListSection() {
 	];
 	return (
 		<div className={`${S.listWrapper}`}>
-			<div className={`${S.recentViewWrapper}`}>
+			<div className={`${S.recentViewWrapper} flex-grow`}>
 				<div className="flex items-center">
 					<h3 className="text-[#dedede] text-3xl">최근 검색어</h3>
-					<button className="text-[1.175rem] font-normal text-[gray] leading-[normal] ml-[1.174rem] inline-flex pt-1">
-						<span>모두 지우기</span>
-						<span
-							style={{
-								backgroundImage: `url(${XCircleIcon})`,
-								position: "relative",
-							}}
-							className="w-[1.175rem] h-[1.175rem] inline-block bg-no-repeat bg-[50%_50%] bg-contain ml-[0.5rem] pt-[0.5rem] leading-normal"
-						></span>
-					</button>
+					{recentSearches.length > 0 && (
+						<button
+							className="text-[1.175rem] font-normal text-[gray] leading-[normal] ml-[1.174rem] inline-flex pt-1"
+							onClick={clearRecentSearches}
+						>
+							<span>모두 지우기</span>
+							<span
+								style={{
+									backgroundImage: `url(${XCircleIcon})`,
+									position: "relative",
+								}}
+								className="w-[1.175rem] h-[1.175rem] inline-block bg-no-repeat bg-[50%_50%] bg-contain ml-[0.5rem] pt-[0.5rem] leading-normal"
+							></span>
+						</button>
+					)}
 				</div>
-				{recentView?.length > 0 ? (
+				{recentSearches.length > 0 ? (
 					<ul className="w-full mt-8">
-						{recentView.map((view) => (
-							<RecentView key={view.id} view={view} />
+						{recentSearches.map((searchTerm, index) => (
+							<RecentView key={index} searchTerm={searchTerm} />
 						))}
 					</ul>
 				) : (
@@ -90,7 +121,7 @@ function ListSection() {
 					</div>
 				)}
 			</div>
-			<div className={`${S.popularViewWrapper}`}>
+			<div className={`${S.popularViewWrapper} flex-grow`}>
 				<h3 className="text-[#dedede] text-3xl">실시간 검색어</h3>
 				<ul className="mt-8">
 					{realtimeSearch &&
@@ -103,152 +134,185 @@ function ListSection() {
 		</div>
 	);
 }
-//@ 현재 시간 표시 컴포넌트
-function CurrentTime() {
-	const now = new Date();
-	const year = now.getFullYear();
-	const month = now.getMonth() + 1; // 월은 0부터 시작하므로 +1을 해줍니다.
-	const date = now.getDate();
 
-	let hours = now.getHours();
-	let minutes = now.getMinutes();
+function SearchResult() {
+	const {
+		contents,
+		allContents,
+		searchData,
+		hasSearched,
+		setSearchData,
+		setHasSearched,
+		fetchData,
+		addRecentSearch,
+	} = useSearchStore();
 
-	// AM/PM 표시를 위해 시간을 조정합니다.
-	const ampm = hours >= 12 ? "오후" : "오전";
+	//@ 페이지네이션 페이징
+	const [isBeginning, setIsBeginning] = useState(true);
+	const [isEnd, setIsEnd] = useState(false);
 
-	//12시간제로 변경
-	hours %= 12;
-	hours = hours ? hours : 12; //0시는 보통 "12"로 표기하기 때문에
+	//@ 현재 카테고리
+	const [currentCategory, setCurrentCategory] = useState(null);
+	//@ 전체보기 여부
+	const [isFullView, setIsFullView] = useState(false);
 
-	// 분이 한 자리수일 경우 앞에 "0"을 붙여줍니다.
-	minutes = minutes < 10 ? "0" + minutes : minutes;
-
-	return (
-		<span className="block mt-4 mb-4 font-[0.75rem] text-[#646464]">
-			{`${year}.${month}.${date} ${ampm} ${hours}:${minutes} 기준`}
-		</span>
-	);
-}
-
-//@ 최근 검색어 컴포넌트
-function RecentView({ view }) {
-	return (
-		<li key={view.id} className="mt-2">
-			<button className="whitespace-nowrap overflow-hidden text-ellipsis cursor-pointer text-[#a6a6a6] text-[1.25rem]">
-				{view.title}
-			</button>
-			<button
-				className="min-w-[1rem] min-h-[1rem] bg-no-repeat bg-[50%_50%] opacity-70 leading-[normal] text-base cursor-pointer align-middle ml-4 pb-2"
-				style={{
-					backgroundImage: `url(${XIcon})`,
-					position: "relative",
-					bottom: "2px",
-				}}
-			></button>
-		</li>
-	);
-}
-//@ 인기 검색어 컴포넌트
-function RealtimeSearch({ item }) {
-	return (
-		<li
-			key={item.id}
-			className="list-none m-2 text-lg text-gray-300 transition-colors duration-100 leading-relaxed"
-		>
-			<button
-				type="button"
-				className="bg-transparent cursor-pointer flex items-center text-gray-400 transition-colors duration-100 ease-linear"
-			>
-				<span className="text-left font-normal text-lg inline-block text-red-500">
-					{item.id}
-				</span>
-				<h3 className="p-0 ml-5 my-1.5 cursor-pointer text-xl text-[#a6a6a6]">
-					{item.title}
-				</h3>
-			</button>
-		</li>
-	);
-}
-
-//@ 검색결과 컴포넌트
-function SearchResult({ status, contents, error }) {
-	if (status === "loading") {
-		return <div>로딩중 입니다...</div>;
-	}
-	if (status === "error") {
+	const handleSearchData = async (content) => {
+		if (!content.title) return;
+		setSearchData(content.title);
+		setHasSearched(true);
+		await fetchData(content.title);
+		addRecentSearch(content.title);
+	};
+	if (searchData && !hasSearched) {
 		return (
-			<div role="alert">
-				<h2>{error.type}</h2>
-				<p>{error.message}</p>
+			<div className={`${S.searchResultWrapper}`}>
+				<Swiper
+					slidesPerView={6}
+					spaceBetween={10}
+					className="mySwiper w-full"
+					breakpoints={{
+						480: { slidesPerView: 5.1 },
+						768: { slidesPerView: 6.1 },
+					}}
+				>
+					{allContents.slice(0, 6).map((content) => (
+						<SwiperSlide key={content.id} className="inline-block h-1/4">
+							<Link to={`/contents/${content.id}`}>
+								<img
+									src={getPbImageURL(content, "poster")}
+									alt={content.title}
+									className="transition-opacity duration-100 ease-in-out inline-block"
+								/>
+								<p className="py-2 mt-1 text-[#dedede] opacity-70 text-base text-left whitespace-nowrap overflow-hidden text-ellipsis">
+									<HighlightedText
+										text={content.title}
+										highlight={searchData}
+									/>
+								</p>
+							</Link>
+						</SwiperSlide>
+					))}
+				</Swiper>
+				<ul className="w-6/12 pl-0 pt-[1rem] list-none">
+					{allContents.slice(6, 9).map((content) => (
+						<li
+							key={content.id}
+							className="text-base text-[#dedede] opacity-70 transition-[color] duration-[0.1s] leading-normal mx-0 my-3 p-0"
+						>
+							<button type="button" onClick={() => handleSearchData(content)}>
+								{" "}
+								<HighlightedText text={content.title} highlight={searchData} />
+							</button>
+						</li>
+					))}
+				</ul>
 			</div>
 		);
-	}
-	return (
-		<div className={`${S.searchResultWrapper}`}>
-			{contents.map(
-				(contentCategory) =>
-					contentCategory.data.length > 0 && (
+	} else if (searchData && hasSearched) {
+		return (
+			<div className={`${S.searchResultWrapper}`}>
+				{contents?.map((contentCategory) =>
+					(!isFullView || currentCategory === contentCategory.title) &&
+					contentCategory.data.length > 0 ? (
 						<div
 							key={contentCategory.title}
 							className={`${S.searchResult} my-5 w-full h-full`}
 						>
 							<h3 className={`${S.resultTitle}`}>{contentCategory.title}</h3>
 							<Swiper
-								slidesPerView={6}
+								slidesPerView={7}
+								slidesPerGroup={7}
 								spaceBetween={10}
-								className="mySwiper w-full"
+								breakpoints={{
+									480: { slidesPerView: 5 },
+									768: { slidesPerView: 6 },
+									1024: { slidesPerView: 7 },
+								}}
+								navigation={{
+									nextEl: "#nextButton",
+									prevEl: "#prevButton",
+									keyboard: {
+										enabled: true,
+										onlyInViewport: false,
+									},
+								}}
+								pagination={{ clickable: true }}
+								modules={[Navigation, Pagination]}
+								onSlideChange={(swiper) => {
+									setIsBeginning(swiper.isBeginning);
+									setIsEnd(swiper.isEnd);
+								}}
+								className={`mySwiper w-full searchResult overflow-y-visible relative pr-7 ${
+									isFullView ? "hide-pagination" : ""
+								}`}
 							>
-								{contentCategory.data.map((content) => (
+								{contentCategory.data.slice(0, 20).map((content) => (
 									<SwiperSlide key={content.id} className="inline-block h-1/4">
-										<img
-											src={getPbImageURL(content, "poster")}
-											alt={content.title}
-											className="transition-opacity duration-100 ease-in-out inline-block"
-										/>
-										<p className="py-2 mt-2 text-white text-lg text-left">
-											{content.title}
-										</p>
+										<Link to={`/contents/${content.id}`}>
+											<img
+												src={getPbImageURL(content, "poster")}
+												alt={content.title}
+												className="transition-opacity duration-100 ease-in-out inline-block"
+											/>
+											<p className="py-2 mt-2 text-[#dedede] opacity-70 text-base text-left whitespace-nowrap overflow-hidden text-ellipsis">
+												{content.title}
+											</p>
+										</Link>
 									</SwiperSlide>
 								))}
+								{!isBeginning && (
+									<div
+										className="swiper-button-prev"
+										id="prevButton"
+										role="button"
+										tabIndex={0}
+										onKeyDown={(e) => {
+											if (e.key === "Enter") e.currentTarget.click();
+										}}
+									/>
+								)}
+								{!isEnd && (
+									<div
+										className="swiper-button-next"
+										id="nextButton"
+										role="button"
+										tabIndex={0}
+										onKeyDown={(e) => {
+											if (e.key === "Enter") e.currentTarget.click();
+										}}
+									/>
+								)}
+								<button
+									type="button"
+									className="seeAllContents"
+									onClick={() => {
+										if (
+											isFullView &&
+											currentCategory === contentCategory.title
+										) {
+											setCurrentCategory(null);
+											setIsFullView(false);
+										} else {
+											setCurrentCategory(contentCategory.title);
+											setIsFullView(true);
+										}
+									}}
+								>
+									{isFullView && currentCategory === contentCategory.title
+										? "전체검색결과 돌아가기"
+										: "전체보기"}
+								</button>
 							</Swiper>
 						</div>
-					)
-			)}
-		</div>
-	);
+					) : null
+				)}
+			</div>
+		);
+	}
 }
 
 function Search() {
-	const [searchData, setSearchData] = useState("");
-	const [contents, setContents] = useState(null);
-	const [status, setStatus] = useState("pending");
-	const [error, setError] = useState(null);
-	const [isSearching, setIsSearching] = useState(false);
-	useEffect(() => {
-		if (!searchData) return; //? 검색어가 없을 경우 요청하지 않습니다.
-
-		setStatus("loading");
-
-		//@ program, movie 컬렉션에서 한 번에 데이터 요청
-		Promise.all([
-			pb
-				.collection("program")
-				.getFullList({ filter: `title ~ "${searchData}"` }),
-			pb.collection("movie").getFullList({ filter: `title ~ "${searchData}"` }),
-		])
-			.then(([programList, movieList]) => {
-				setContents([
-					{ title: "TV 프로그램", data: programList },
-					{ title: "영화", data: movieList },
-				]);
-				setStatus("success");
-			})
-			.catch((error) => {
-				setError(error);
-				setStatus("error");
-			});
-	}, [searchData]);
-
+	const { searchData } = useSearchStore();
 	return (
 		<>
 			<Helmet>
@@ -256,16 +320,8 @@ function Search() {
 			</Helmet>
 			<div className={`${S.container}`}>
 				<div className={`${S.main} `}>
-					<SearchSection
-						searchData={searchData}
-						setSearchData={setSearchData}
-						setIsSearching={setIsSearching}
-					/>
-					{isSearching ? (
-						<SearchResult status={status} contents={contents} error={error} />
-					) : (
-						<ListSection />
-					)}
+					<SearchSection />
+					{searchData ? <SearchResult /> : <ListSection />}
 				</div>
 			</div>
 		</>
@@ -273,17 +329,17 @@ function Search() {
 }
 
 //RecentView.propTypes = {
-//	view: propTypes.shape({
-//		id: number.isRequired,
-//		title: string.isRequired,
-//	}),
+//  view: propTypes.shape({
+//    id: number.isRequired,
+//    title: string.isRequired,
+//  }),
 //};
 
 //RealtimeSearch.propTypes = {
-//	item: PropTypes.shape({
-//		id: number.isRequired,
-//		title: string.isRequired,
-//	}),
+//  item: PropTypes.shape({
+//    id: number.isRequired,
+//    title: string.isRequired,
+//  }),
 //};
 
 export default Search;
