c85559529a331355d409a4c5c6c698e56c774c10
o0orang <esksk7942@naver.com>
2023-09-13 21:57:39 +0900

    ➕ [add] : 파일 추가 // => 제목
    favorite.jsx, favorite.module.css 파일 추가
    
    fixes: 추후 수정

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,8 +1,13 @@
+import S from "./Favorite.module.css";
 
 function Favorite() {
-  return (
-    <div>Favorite</div>
-  )
+	return (
+		<main className={S.main}>
+			<section className={S.section}>
+				<p className="text-xl font-extrabold">내가 찜한 콘텐츠</p>
+			</section>
+		</main>
+	);
 }
 
-export default Favorite
+export default Favorite;
