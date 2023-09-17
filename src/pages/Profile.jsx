5af5033470fa7967c6b06dad7aa95de607a779b7
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-09-17 20:39:50 +0900

    ✨ [feat] 프로필 페이지 레이아웃 수정
    프로필 전환 페이지 레이아웃을 일부 수정했습니다.
    추후 로그인 기능 구현에 따라 기능 구현 예정입니다.
    
    fixes: 추후 반응형 디자인으로 변경 소요 있음
    Related to #39

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -12,13 +12,13 @@ function Profile() {
 	return (
 		<section className="bg-black w-screen h-screen flex items-center justify-center text-white my-auto relative pt-[2rem] lg:pt-[1.5rem] md:pt-[1rem]">
 			<div className="flex flex-col justify-center items-center min-h-full gap-12">
-				<div className="flex flex-col gap-1">
-					<h2 className="text-2xl text-center font-bold">프로필 선택</h2>
-					<p className="box-border text-neutral-400 text-base break-keep mt-2 px-8 py-0">
+				<div className="flex flex-col gap-2">
+					<h2 className="text-3xl text-center font-bold">프로필 선택</h2>
+					<p className="box-border text-neutral-400 text-lg break-keep mt-2 px-8 py-0">
 						시청할 프로필을 선택해주세요.
 					</p>
 				</div>
-				<ul className="w-full flex items-center justify-center gap-1">
+				<ul className="flex items-center justify-center gap-7 w-2/3">
 					{profiles.map((profile) => (
 						<li
 							key={profile.name}
@@ -26,7 +26,7 @@ function Profile() {
 						>
 							<button
 								type="button"
-								className="border-solid border-white border-4 block w-2/6 h-2/6 overflow-hidden transition-all duration-[0.3s] p-0 rounded-[3px]"
+								className="border-solid border-white border-4 block w-3/7 h-3/7 overflow-hidden transition-all duration-[0.3s] p-0 rounded-[3px]"
 							>
 								<img
 									src={profile.imgSrc}
@@ -34,13 +34,13 @@ function Profile() {
 									className="w-full h-full object-cover"
 								/>
 							</button>
-							<p className="text-xs text-neutral-400 my-2">{profile.name}</p>
+							<p className="text-sm text-neutral-400 my-2">{profile.name}</p>
 						</li>
 					))}
 				</ul>
 				<button
 					type="button"
-					className="block w-1/3 box-border border bg-black text-neutral-400 text-base text-center px-0 py-3 border-solid border-[#4e4e4e] rounded"
+					className="block w-1/4 box-border border bg-black text-neutral-400 text-lg text-center px-0 py-3 border-solid border-[#4e4e4e] rounded"
 					onClick={() => navigate("/editprofiles")}
 				>
 					프로필 편집
