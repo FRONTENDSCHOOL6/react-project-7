1b527a8536bc011286d69ad2e251134a7caaad58
Sirori <tldus7519@gmail.com>
2023-09-18 17:13:33 +0900

    ✨ [feat] 헤더 프로필 호버 이벤트 추가
    프로필 이미지에 hover시 팝업이 뜨는 기능 추가
    
    fixes: 이벤트 오류가 있어 수정 예정

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,23 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import S from "./Header.module.css";
 import logo from "/assets/logo.svg";
 import searchIcon from "/assets/search.png";
 import profileIcon from "/assets/profile.png";
+import useStorage from "@/hooks/useStorage";
+import authStore from "@/store/authStore";
+import { useCallback } from "react";
 
 function Header() {
 	const [isScrolled, setIsScrolled] = useState(false);
+	const [isHovered, setIsHovered] = useState(false);
+	const [showLogoutPopup, setShowLogoutPopup] = useState(false);
+
+	const { authState, signOut } = authStore(); // authState와 signOut을 가져옵니다.
+
+	console.log(authState);
+
+	const navigate = useNavigate();
 
 	//@ 스크롤 이벤트
 	useEffect(() => {
@@ -25,6 +36,23 @@ function Header() {
 		};
 	}, []);
 
+	useEffect(() => {
+		const storageData = localStorage.getItem("pocketbase_auth");
+		if (storageData) {
+			const parsedData = JSON.parse(storageData);
+			authStore.setState({ authState: parsedData });
+		}
+	}, []);
+
+	const handleHover = () => {
+		setIsHovered(isHovered);
+	};
+	const handleLogoutClick = () => {
+		handleShowLogoutPopup();
+	};
+	const handleShowLogoutPopup = () => {
+		setShowLogoutPopup(!showLogoutPopup);
+	};
 	return (
 		<header
 			className={`
@@ -59,12 +87,83 @@ function Header() {
 						<img src={searchIcon} alt="검색" className={S.profileImg} />
 					</Link>
 				</li>
-				<li>
+				<li onMouseEnter={handleHover} onMouseLeave={handleHover}>
 					<Link to="profile">
 						<img src={profileIcon} alt="프로필" className={S.profileImg} />
 					</Link>
 				</li>
 			</ul>
+			<div
+				className={`leading-[1.15] text-base text-white fixed min-w-[15rem] box-border shadow-[0px_5px_10px_0_rgba(0,0,0,0.5)] bg-[#212121] translate-x-0 -translate-y-2.5 -mt-0.5 pt-6 pb-[1.167rem] px-0 rounded-sm border-solid border-[#4d4d4d] border right-[3.5rem] top-[5.833rem] transition-all z-50 
+          ${isHovered ? "" : "invisible opacity-0"}`}
+				onMouseEnter={handleHover}
+				onMouseLeave={handleHover}
+			>
+				<div className="flex flex-col px-5">
+					<div className="flex gap-3">
+						<img src={profileIcon} alt="유저의 프로필 이미지" />
+						<div className="flex flex-col">
+							<span className="font-semibold text-left">shclgus2</span>
+
+							<button
+								type="button"
+								className="pt-1 text-sm text-left"
+								onClick={() => navigate("/profile")}
+							>
+								<span className="text-sm text-[#a3a3a3] hover:text-white">
+									프로필 전환 &gt;
+								</span>
+							</button>
+						</div>
+					</div>
+				</div>
+				<hr className="border-[#2e2e2e] border-t h-[0.0625rem] bg-[#2e2e2e] my-4" />
+				<ul className="py-2 flex flex-col justify-center gap-3">
+					<li
+						className="inline-block w-full text-lg leading-normal text-neutral-400 transition-[color] duration-[0.1s] px-[1.667rem] py-2 hover:text-white hover:bg-[#2e2e2e]"
+						onClick={() => navigate("/membership")}
+					>
+						이용권
+					</li>
+					<li
+						className="inline-block w-full text-lg leading-normal text-neutral-400 transition-[color] duration-[0.1s] px-[1.667rem] py-2 hover:text-white hover:bg-[#2e2e2e]"
+						onClick={handleLogoutClick}
+					>
+						로그아웃
+					</li>
+				</ul>
+			</div>
+			{showLogoutPopup && (
+				<div className="fixed inset-0 bg-black opacity-[95%]">
+					<div className="absolute top-[calc(50vh_-_100px)] left-[calc(50vw_-_200px)] bg-[#212121] flex justify-center items-center w-[25rem] h-[9.375rem] rounded flex-col">
+						<h2 className="py-4 px-4 text-white text-xl inline-block">
+							로그아웃 하시겠습니까?
+						</h2>
+						<div className="flex justify-between items-center w-full mt-4">
+							<button
+								type="button"
+								className="text-[#a3a3a3] flex-grow hover:text-white"
+								onClick={async () => {
+									await signOut();
+									localStorage.removeItem("pocketbase_auth");
+									setShowLogoutPopup(false);
+									navigate("/onboarding");
+								}}
+							>
+								확인
+							</button>
+							<span className="text-[#a3a3a3]">|</span>
+							<button
+								type="button"
+								className="text-[#a3a3a3] flex-grow hover:text-white"
+								onClick={handleShowLogoutPopup}
+							>
+								취소{" "}
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
 		</header>
 	);
 }
