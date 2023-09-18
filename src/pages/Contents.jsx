a4b790fdfd38655532762ee8cc6d5567c57d6b29
o0orang <esksk7942@naver.com>
2023-09-18 14:38:26 +0900

    ✨ [feat] : 상세 페이지 새로운 기능 추가
    에피소드 정렬, 포켓베이스 데이터 read, 리뷰 수정과 삭제 기능 추가
    
    fixs : 추후 수정 가능성 있음

--- src/pages/Contents.jsx
+++ src/pages/Contents.jsx
@@ -2,13 +2,21 @@ import S from "./Contents.module.css";
 import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
+import { getPbImageURL } from "@/utils/getPbImageURL";
+import pb from "@/api/pocketbase";
+import removeQuotes from "/react-project-7/src/utils/removeQuotes.js";
+import separateComma from "/react-project-7/src/utils/separateComma.js";
 import PocketBase from "pocketbase";
 import { useParams } from "react-router-dom";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import TwitterIcon from "/assets/twitter-logo.png";
+import ShareIcon from "/assets/share-logo.png";
+import FacebookIcon from "/assets/facebook-logo.png";
 
 function Contents() {
+	//@ 상태 객체 처리
 	const [state, setState] = useState({
 		showFullDescription: false,
 		title: "",
@@ -19,17 +27,30 @@ function Contents() {
 		description: "",
 		release: "",
 		rating: "",
+		version: "",
 		season: false,
 		subtitle: false,
 		isDRM: false,
 		runningTime: false,
+		episodes: [],
+		similar: [],
+		related: [],
+		episodeTitles: "",
+		episodeThumbs: [],
+		similarThumbs: [],
+		relatedTitles: "",
 	});
 
-	//@ 이미지 url 생성 함수
-	const getPbImageURL = (item, fileName = "photo") =>
-		`${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${item.id}/${
-			item[fileName]
-		}`;
+	const ratingText = {
+		all: "전체 이용가",
+		eighteen: "18세",
+		twelve: "12세",
+		fifteen: "15세",
+		seven: "7세",
+	};
+	const versionText = {
+		dubbed: "더빙판",
+	};
 
 	//@ 포켓베이스 데이터 가져오기
 	const { id } = useParams();
@@ -40,13 +61,61 @@ function Contents() {
 					`${import.meta.env.VITE_PB_API}/collections/${type}/records/${id}`
 				);
 				const data = await response.json();
-
-				//@ 이미지 데이터 가져오기
+				console.log(`${type} data:`, data);
+				let newState = {};
+				// 회차 정보
 				if (data.poster) {
-					setState((prevState) => ({
-						...prevState,
-						poster: getPbImageURL(data, "poster"),
-					}));
+					newState.poster = getPbImageURL(data, "poster");
+				}
+				if (data.episodeThumbs && data.episodeTitles && data.episodeInfos) {
+					const episodeThumbs = data.episodeThumbs.map((imgName) =>
+						getPbImageURL(
+							{
+								collectionId: data.collectionId,
+								id: data.id,
+								[imgName]: imgName,
+							},
+							imgName
+						)
+					);
+					const episodeTitles = data.episodeTitles.split(",");
+					const episodeInfos = data.episodeInfos.split(",");
+					const episodes = [];
+					for (let i = 0; i < episodeThumbs.length; i++) {
+						if (episodeInfos[i]) {
+							//? episodeInfos가 존재할 때만 넣기
+							episodes.push({
+								thumbUrl: episodeThumbs[i],
+								title: episodeTitles[i],
+								info: episodeInfos[i],
+							});
+						}
+					}
+					newState.episodes = episodes;
+				}
+				// 비슷한 프로그램
+				if (data.similarThumbs && data.similarInfos) {
+					const similarThumbs = data.similarThumbs.map((imgName) =>
+						getPbImageURL(
+							{
+								collectionId: data.collectionId,
+								id: data.id,
+								[imgName]: imgName,
+							},
+							imgName
+						)
+					);
+					const similarInfos = data.similarInfos.split(",");
+					const similar = [];
+					for (let i = 0; i < similarThumbs.length; i++) {
+						if (similarInfos[i]) {
+							similar.push({
+								thumbUrl: similarThumbs[i],
+								info: similarInfos[i],
+							});
+						}
+					}
+					newState.similar = similar;
 				}
 
 				const keys = [
@@ -58,174 +127,163 @@ function Contents() {
 					"release",
 					"season",
 					"subtitle",
+					"rating",
 					"isDRM",
+					"episodeTitles",
+					"version",
 				];
 				for (let key of keys) {
 					if (data[key]) {
-						setState((prevState) => ({ ...prevState, [key]: data[key] }));
+						newState[key] = data[key];
 					}
+				}
+				if (type === "movie" && data.runningTime) {
+					newState.runningTime = data.runningTime;
+				}
+				if (type === "movie" && data.version) {
+					newState.version = data.version;
 				}
 
-				if (type === "movie") {
-					if (data.rating) {
-						setState((prevState) => ({ ...prevState, rating: data.rating }));
-					}
-					if (data.runningTime) {
-						setState((prevState) => ({
-							...prevState,
-							runningTime: data.runningTime,
-						}));
-					}
-				}
+				return newState;
 			} catch (error) {
 				console.error(`Failed to fetch ${type} data`, error);
 			}
 		};
 
-		handleData("program");
-		handleData("movie");
+		Promise.all([handleData("program"), handleData("movie")])
+			.then(([programState, movieState]) => {
+				//? 최신의 state 값으로 새로운 state
+				setState((prevState) => ({
+					...prevState,
+					...programState,
+					...movieState,
+				}));
+			})
+			.catch((error) => console.error(error));
 	}, [id]);
+	//@ 공유하기
+	const handleShare = () => {
+		navigator.clipboard.writeText(window.location.href);
+		alert("링크가 복사되었습니다!");
+	}; //? 공유 버튼 클릭시 현재 페이지 주소를 클립보드에 복사
 
-	// review 기능 구현
+	const shareToFacebook = () => {
+		const currentUrl = window.location.href;
+		const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+			currentUrl
+		)}`;
+		window.open(facebookShareUrl, "_blank");
+	};
+
+	const shareToTwitter = () => {
+		const currentUrl = window.location.href;
+		const text = "티빙! 오리지널을 만나보세요.";
+		const twitterShareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+			text
+		)}&url=${encodeURIComponent(currentUrl)}`;
+		window.open(twitterShareUrl, "_blank");
+	};
+
+	const [isShareOpen, setIsShareOpen] = useState(false);
+
+	//@ review
 	const [stars, setStars] = useState(0); //? 현재 별점
 	const [hoverRating, setHoverRating] = useState(0); //? 마우스 호버시 별점
 	const [reviewText, setReviewText] = useState(""); //? 현재 입력중인 리뷰
 	const [reviews, setReviews] = useState([]); //? 저장된 리뷰
+	const [editingReviewIndex, setEditingReviewIndex] = useState(null);
+	const [editedComment, setEditedComment] = useState("");
 
+	// 리뷰 작성
 	const handleReviewChange = (e) => {
-		setReviewText(e.target.value);
-	}; //? e.target.value를 reviewText에 저장
+		if (editingReviewIndex !== null) {
+			setEditedComment(e.target.value);
+		} else {
+			setReviewText(e.target.value);
+		}
+	};
 
 	const handleReviewSubmit = (e) => {
 		e.preventDefault();
-		if (reviewText !== "" && stars !== null) {
-			setReviews([...reviews, { text: reviewText, stars: stars }]);
+
+		if ((reviewText !== "" || editedComment !== "") && stars !== null) {
+			const newReviews = [...reviews];
+
+			if (editingReviewIndex !== null) {
+				newReviews[editingReviewIndex].text = editedComment;
+				newReviews[editingReviewIndex].stars = stars;
+				setEditingReviewIndex(null);
+				setEditedComment("");
+			} else {
+				newReviews.push({ text: reviewText, stars: stars });
+				setStars(0);
+				setHoverRating(0);
+			}
+			setReviews(newReviews);
 			setReviewText("");
 		}
-	}; //? 리뷰 제출 클릭시 호출
+	};
 
-	// 찜 기능 구현
+	// 수정
+	const handleEditClick = (index) => {
+		const reviewToEdit = reviews[index];
+		setEditingReviewIndex(index);
+		if (reviewToEdit) {
+			setEditedComment(reviewToEdit.text);
+			setStars(reviewToEdit.stars);
+		}
+	};
+	// 삭제
+	const handleDeleteClick = (index) => {
+		setReviews(reviews.filter((_, i) => i !== index));
+	};
+
+	//@ 찜하기
 	const [isChanged, setChanged] = useState(false); //? 찜 버튼 상태
-
 	const handleHeart = () => {
 		setChanged(!isChanged);
 	}; //? 찜 버튼 클릭시 호출
 
-	// 연속 재생 토글 기능 구현
+	//@ 연속 재생
 	const [isToggled, setIsToggled] = useState(false); //? 연속 재생 버튼 상태
 
 	const handleClick = () => {
 		setIsToggled(!isToggled);
 	}; //? 연속 재생 토글 버튼 클릭시 호출
 
-	// 에피소드 정렬 기능 구현
+	//@ 에피소드 정렬 기능 구현
 	const [sortKey, setSortKey] = useState(0); //? 에피소드 정렬 후 리렌터링
 	const [isSorted, setIsSorted] = useState(true); //? 에피소드 정렬 방식, true:첫화부터
 
+	// 내림차순
 	const handleSortAsc = () => {
-		const sortedEpisodes = [...episodes].sort((a, b) => {
-			const titleA = parseInt(a.title.match(/^\d+/)); //? 에피소드 번호 추출
-			const titleB = parseInt(b.title.match(/^\d+/));
+		const sortedEpisodes = [...state.episodes].sort((a, b) => {
+			const titleA = parseInt(a.title.split(",")[0]);
+			const titleB = parseInt(b.title.split(",")[0]);
+			return titleA - titleB;
+		});
 
-			return titleA - titleB; //? 오름차순 정렬
-		}); //? 첫화부터 정렬 버튼 클릭시 호출
-
-		setEpisodes(sortedEpisodes);
-		setIsSorted(true); //? 첫화부터 버튼
-		setSortKey((prevKey) => prevKey + 1); //? 정렬 후 key 값 변경하여 리렌더링 trigger
+		setState((prevState) => ({
+			...prevState,
+			episodes: sortedEpisodes,
+		}));
+		setIsSorted(true);
 	};
 
+	// 오름차순
 	const handleSortDesc = () => {
-		const sortedEpisodes = [...episodes].sort((a, b) => {
-			const titleA = parseInt(a.title.match(/^\d+/));
-			const titleB = parseInt(b.title.match(/^\d+/));
+		const sortedEpisodes = [...state.episodes].sort((a, b) => {
+			const titleA = parseInt(a.title.split(",")[0]);
+			const titleB = parseInt(b.title.split(",")[0]);
+			return titleB - titleA;
+		});
 
-			return titleB - titleA; //? 내림차순 정렬
-		}); //? 최신화부터 정렬 버튼 클릭시 호출
-
-		setEpisodes(sortedEpisodes);
-		setIsSorted(false); //? 최신화부터 버튼
-		setSortKey((prevKey) => prevKey + 1);
+		setState((prevState) => ({
+			...prevState,
+			episodes: sortedEpisodes,
+		}));
+		setIsSorted(false);
 	};
-
-	// 공유 기능 구현
-	const handleShare = () => {
-		navigator.clipboard.writeText(window.location.href);
-		alert("링크가 복사되었습니다!");
-	}; //? 공유 버튼 클릭시 현재 페이지 주소를 클립보드에 복사
-
-	// 에피소드 정보 저장
-	const [episodes, setEpisodes] = useState([
-		{
-			title: "1. 알쓸별잡 1화",
-			detail: "상세 정보",
-			date: "2023.08.03",
-			duration: "98분",
-			imgSrc: "/contents/ep1.png",
-		},
-		{
-			title: "2. 알쓸별잡 2화",
-			detail: "상세 정보",
-			date: "2023.08.10",
-			duration: "92분",
-			imgSrc: "/contents/ep2.png",
-		},
-		{
-			title: "3. 알쓸별잡 3화",
-			detail: "상세 정보",
-			date: "2023.08.17",
-			duration: "101분",
-			imgSrc: "/contents/ep3.png",
-		},
-		{
-			title: "4. 알쓸별잡 4화",
-			detail: "상세 정보",
-			date: "2023.08.24",
-			duration: "95분",
-			imgSrc: "/contents/ep4.png",
-		},
-		{
-			title: "5. 알쓸별잡 5화",
-			detail: "상세 정보",
-			date: "2023.08.31",
-			duration: "96분",
-			imgSrc: "/contents/ep2.png",
-		},
-	]);
-
-	// 관련 컨텐츠 정보 배열
-
-	const related = [
-		{ title: "알쓸신잡", imgSrc: "/contents/relate1.png" },
-		{ title: "동네의 사생활", imgSrc: "/contents/relate2.png" },
-		{ title: "차이나피디아", imgSrc: "/contents/relate3.png" },
-		{
-			title: "잡학다식한 남자들의 히든카드 M16",
-			imgSrc: "/contents/relate4.png",
-		},
-		{ title: "만국견문록", imgSrc: "/contents/relate5.png" },
-		{ title: "렛츠고", imgSrc: "/contents/relate6.png" },
-		{ title: "동네의 사생활", imgSrc: "/contents/relate2.png" },
-		{ title: "알쓸신잡", imgSrc: "/contents/relate1.png" },
-		{ title: "동네의 사생활", imgSrc: "/contents/relate2.png" },
-		{ title: "차이나피디아", imgSrc: "/contents/relate3.png" },
-		{
-			title: "잡학다식한 남자들의 히든카드 M16",
-			imgSrc: "/contents/relate4.png",
-		},
-		{ title: "만국견문록", imgSrc: "/contents/relate5.png" },
-		{ title: "렛츠고", imgSrc: "/contents/relate6.png" },
-		{ title: "동네의 사생활", imgSrc: "/contents/relate2.png" },
-		{ title: "만국견문록", imgSrc: "/contents/relate5.png" },
-		{ title: "렛츠고", imgSrc: "/contents/relate6.png" },
-		{ title: "동네의 사생활", imgSrc: "/contents/relate2.png" },
-		{ title: "만국견문록", imgSrc: "/contents/relate5.png" },
-		{ title: "렛츠고", imgSrc: "/contents/relate6.png" },
-		{ title: "동네의 사생활", imgSrc: "/contents/relate2.png" },
-		{ title: "만국견문록", imgSrc: "/contents/relate5.png" },
-		{ title: "렛츠고", imgSrc: "/contents/relate6.png" },
-		{ title: "동네의 사생활", imgSrc: "/contents/relate2.png" },
-	];
 
 	return (
 		<main className={`text-gray300 ${S.main} w-screen overflow-hidden`}>
@@ -237,14 +295,18 @@ function Contents() {
 					/>
 				</div>
 				<div className="relative z-10 flex flex-row justify-between h-full">
-					<div className="w-3/5">
-						<h2 className="mb-[1.25rem] text-5xl font-bold text-white">
+					<div className="w-2/5">
+						<h2 className="mb-[1.25rem] text-5xl font-semibold text-white">
 							{state.title}
 						</h2>
 						<div className="border-gray300 ">
 							<span className={S.tag}>{state.release}</span>
+							<span className={S.tag}>{ratingText[state.rating]}</span>
 							{state.runningTime && (
 								<span className={S.tag}>{state.runningTime}분</span>
+							)}
+							{state.version !== "X" && state.version && (
+								<span className={S.tag}>{versionText[state.version]}</span>
 							)}
 							{state.producer && (
 								<span className={S.tag}>{state.producer}</span>
@@ -262,21 +324,48 @@ function Contents() {
 							<button className={S.likeBtn} type="submit" onClick={handleHeart}>
 								<img
 									className="m-auto w-[2rem] h-[2rem] mt-[0.5625rem] mb-[0.3125rem] "
-									src={
-										isChanged ? "/assets/full-heart.svg" : "/assets/heart.svg"
-									}
+									src={isChanged ? "assets/full-heart.svg" : "assets/heart.svg"}
 									alt="하트 버튼"
 								/>
 								<span className="text-white">찜</span>
 							</button>
-							<button className={S.likeBtn} type="submit" onClick={handleShare}>
-								<img className={S.shareBtn} alt="공유 버튼" />
-								<span className="text-white">공유</span>
-							</button>
+							<div className="relative">
+								<button
+									className={S.likeBtn}
+									type="submit"
+									onClick={() => setIsShareOpen(!isShareOpen)}
+								>
+									<img className={S.shareBtn} alt="공유 버튼" />
+									<span className="text-white">공유</span>
+								</button>
+								{isShareOpen && (
+									<div
+										className={`${S.sharePopup} flex justify-around items-center`}
+									>
+										<img
+											className="w-[25%] h-[65%] "
+											src={ShareIcon}
+											onClick={handleShare}
+										></img>
+										<img
+											className="w-[25%] h-[65%]"
+											src={FacebookIcon}
+											onClick={shareToFacebook}
+										/>
+										<img
+											className="w-[25%] h-[65%]"
+											src={TwitterIcon}
+											onClick={shareToTwitter}
+										/>
+									</div>
+								)}
+							</div>
 						</div>
 						<dl className="flex mt-[1.5625rem] font-semibold">
-							<dt className="mr-[0.9375rem] font-semibold">크리에이터</dt>
-							<dd>{state.creator}</dd>
+							<dt className="mr-[0.9375rem] whitespace-nowrap font-semibold">
+								크리에이터
+							</dt>
+							<dd className={S.truncate}>{state.creator}</dd>
 						</dl>
 						<dl className="flex font-semibold">
 							<dt className="whitespace-nowrap mr-[0.9375rem]">출연</dt>
@@ -312,11 +401,16 @@ function Contents() {
 					<hr className="border-gray400" />
 					<div className="flex justify-between mt-[1.9375rem] mb-[1.125rem]">
 						<button className={S.sectionTitle} type="submit">
-							알쓸별잡{" "}
-							<span className="text-base font-semibold text-gray500">
-								{" "}
-								&#40;총 5화&#41;{" "}
-							</span>{" "}
+							{state.title}
+							<div>
+								{state.episodeThumbs.map((imgUrl, index) => (
+									<img
+										key={index}
+										src={imgUrl}
+										alt={`Episode Thumbnail ${index}`}
+									/>
+								))}
+							</div>
 						</button>
 						<div className="pr-[1rem] flex justify-end font-semibold">
 							<button
@@ -356,14 +450,19 @@ function Contents() {
 							</div>
 						</div>
 					</div>
-					<div className=" w-full font-semibold">
+					<div className=" w-full font-normal">
 						<Swiper
-							className="px-[3rem] detailSwiper"
+							className="px-[3rem] detailSwiper swiperpagination-progressbar"
 							key={sortKey}
 							spaceBetween={10}
-							modules={[Navigation]}
+							modules={[Pagination, Navigation]}
 							slidesPerView={4}
 							tabIndex={0}
+							breakpoints={{
+								480: { slidesPerView: 2 },
+								768: { slidesPerView: 3 },
+								1024: { slidesPerView: 4 },
+							}}
 							navigation={{
 								nextEl: "#nextButton",
 								prevEl: "#prevButton",
@@ -373,16 +472,18 @@ function Contents() {
 								},
 							}}
 						>
-							{episodes.map((episode, index) => (
+							{state.episodes.map((episode, index) => (
 								<SwiperSlide key={index}>
-									<figure className={`pt-[0.5625rem]  ${S.figureHoverEffect}`}>
-										<img className=" " src={episode.imgSrc} />
+									<figure className={`pt-[0.5625rem] ${S.figureHoverEffect}`}>
+										<img
+											src={episode.thumbUrl}
+											alt={`Episode Thumbnail ${index}`}
+										></img>
 										<figcaption className=" pt-[0.5625rem]">
-											<h4 className="text-white text-lg">{episode.title}</h4>
-											<p>{episode.detail}</p>
-											<span className="text-gray500">
-												{episode.date} | {episode.duration}
-											</span>
+											<h4 className="text-gray100 text-base font-semibold">
+												{episode.title}
+											</h4>
+											<p>{episode.info}</p>
 										</figcaption>
 									</figure>
 								</SwiperSlide>
@@ -410,7 +511,7 @@ function Contents() {
 				</section>
 				<section className={S.section}>
 					<span className={S.sectionTitle}>비슷한 TV 프로그램</span>
-					<div className="flex justify-between w-full font-semibold ">
+					<div className="flex justify-between w-full font-normal ">
 						<Swiper
 							className="detailSwiper detailPagenation px-[3rem]  overflow-y-visible   "
 							key={sortKey}
@@ -420,6 +521,11 @@ function Contents() {
 							slidesPerGroup={7}
 							tabIndex={0}
 							freeMode={true}
+							breakpoints={{
+								480: { slidesPerView: 5 },
+								768: { slidesPerView: 6 },
+								1024: { slidesPerView: 7 },
+							}}
 							navigation={{
 								nextEl: "#nextButton",
 								prevEl: "#prevButton",
@@ -432,12 +538,15 @@ function Contents() {
 								clickable: true,
 							}}
 						>
-							{related.map((program, index) => (
+							{state.similar.map((similar, index) => (
 								<SwiperSlide key={index}>
 									<figure className={`pt-[0.5625rem] ${S.figureHoverEffect}`}>
-										<img className="" src={program.imgSrc} />
-										<figcaption className=" pt-[0.5625rem]">
-											<h4 className="text-lg break-words">{program.title}</h4>
+										<img
+											src={similar.thumbUrl}
+											alt={`Episode Thumbnail ${index}`}
+										></img>
+										<figcaption className="font-semibold pt-[0.5625rem]">
+											<h4>{similar.info}</h4>
 										</figcaption>
 									</figure>
 								</SwiperSlide>
@@ -504,12 +613,42 @@ function Contents() {
 					<div>
 						<ul className="w-[50%]">
 							{reviews.map((review, index) => (
-								<li key={index} className={S.reviewBox}>
-									<span style={{ fontSize: "0.8rem" }}>{`${"⭐".repeat(
-										review.stars
-									)}`}</span>
-									<br />
-									{review.text}
+								<li className="pl-[3rem]" key={index}>
+									<span>{`${"⭐".repeat(review.stars)}`}</span>
+									{index === editingReviewIndex ? (
+										<div>
+											<input
+												className={S.editInput}
+												value={editedComment}
+												onChange={handleReviewChange}
+											/>
+											<button
+												className="ml-[0.3125rem]  mr-[0.3125rem] "
+												onClick={handleReviewSubmit}
+											>
+												수정완료
+											</button>
+											<button onClick={() => setEditingReviewIndex(null)}>
+												취소
+											</button>
+										</div>
+									) : (
+										<>
+											{review.text}
+											<button
+												className={S.editBtn}
+												onClick={() => handleEditClick(index)}
+											>
+												수정
+											</button>
+											<button
+												className={S.editBtn}
+												onClick={() => handleDeleteClick(index)}
+											>
+												삭제
+											</button>
+										</>
+									)}
 								</li>
 							))}
 						</ul>
