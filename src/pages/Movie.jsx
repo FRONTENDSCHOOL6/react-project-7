75132258908b5d1c3fc887b82d931353843c241c
Sirori <tldus7519@gmail.com>
2023-09-20 01:16:05 +0900

    ♻️ [refactor] Helmet 태그 추가
    페이지마다 Helmet 태그 추가
    
    fixes: 반응형 레이아웃 수정 예정

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import S from "./Movie.module.css";
 import { Link } from "react-router-dom";
 import React, { useRef, useEffect, useState } from "react";
@@ -5,11 +6,16 @@ import { MovieNav, MovieContent } from "./MovieNav";
 
 export default function Movie() {
 	return (
-		<div className="bg-black pt-40 px-[5%]">
-			<MovieNav />
-			<div className="contentWrapper w-full pt-10 flex flex-row gap-5 flex-wrap justify-between">
-				<MovieContent />
+		<>
+			<Helmet>
+				<title>Movies - Taing</title>
+			</Helmet>
+			<div className="bg-black pt-40 px-[5%]">
+				<MovieNav />
+				<div className="contentWrapper w-full pt-10 flex flex-row gap-5 flex-wrap justify-between">
+					<MovieContent />
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import S from "./Program.module.css";
 import { Link } from "react-router-dom";
 import React, { useRef, useEffect, useState } from "react";
@@ -5,11 +6,16 @@ import { ProgramNav, ProgramContent } from "./ProgramNav";
 
 export default function Movie() {
 	return (
-		<div className="bg-black pt-40 px-[5%]">
-			<ProgramNav />
-			<div className="contentWrapper w-full pt-10 flex flex-row gap-5 flex-wrap justify-between">
-				<ProgramContent />
+		<>
+			<Helmet>
+				<title>TV Programs - Taing</title>
+			</Helmet>
+			<div className="bg-black pt-40 px-[5%]">
+				<ProgramNav />
+				<div className="contentWrapper w-full pt-10 flex flex-row gap-5 flex-wrap justify-between">
+					<ProgramContent />
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
