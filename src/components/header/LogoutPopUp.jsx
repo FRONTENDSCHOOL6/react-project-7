0efdb6dd84cd11770678624f37314baefd694045
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-09-23 22:45:06 +0900

    ♻️ [refactor] 검색 페이지 메타태그 추가
    검색 페이지에 헬멧을 이용해 메타태그를 넣었습니다.
    그 외에 일부 코드 변경사항 있습니다.
    
    fixes: 추후 리팩토링으로 구조 변경 가능성 있음

---
+++ src/components/header/LogoutPopUp.jsx
@@ -0,0 +1,48 @@
+import { useNavigate } from "react-router-dom";
+import authStore from "@/store/useAuthStore";
+import { func } from "prop-types";
+import S from "@/components/header/Header.module.css";
+export default function LogoutPopUp({
+	setShowLogoutPopup,
+	handleShowLogoutPopup,
+}) {
+	const navigate = useNavigate();
+	const { signOut } = authStore();
+	return (
+		<div className="fixed inset-0 bg-black opacity-[100%]">
+			<div className={S.logoutWrapper}>
+				<h2 className="py-4 px-4 text-white text-xl inline-block">
+					로그아웃 하시겠습니까?
+				</h2>
+				<div className="flex justify-between items-center w-full mt-4">
+					<button
+						type="button"
+						className="text-[#a3a3a3] flex-grow hover:text-white"
+						onClick={async () => {
+							await signOut();
+							localStorage.removeItem("pocketbase_auth");
+							localStorage.removeItem("selectedProfile");
+							setShowLogoutPopup(false);
+							navigate("/onboarding");
+						}}
+					>
+						확인
+					</button>
+					<span className="text-[#a3a3a3]">|</span>
+					<button
+						type="button"
+						className="text-[#a3a3a3] flex-grow hover:text-white"
+						onClick={handleShowLogoutPopup}
+					>
+						취소{" "}
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+LogoutPopUp.propTypes = {
+	setShowLogoutPopup: func,
+	handleShowLogoutPopup: func,
+};
